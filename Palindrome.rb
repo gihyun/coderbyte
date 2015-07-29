23ecba8ca485e78b7e8600081e08bfbb011a2d01
kyunni <kghgy4022@gmail.com>
2015-07-29 13:22:11 +0900

    Update Palindrome.rb

--- Palindrome.rb
+++ Palindrome.rb
@@ -1,7 +1,14 @@
 def Palindrome(str)
 
-  # code goes here
-  return str == str.reverse
+  aComp = str.split.join
+  
+  if aComp == aComp.reverse
+    flag = true
+  else
+    flag = false
+  end
+  
+  return flag
          
 end
    
